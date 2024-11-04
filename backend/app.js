2b31027054a6f9180ec92c5479a930d10aefd2a7
stevensusas <hanqisu@sas.upenn.edu>
2024-11-03 19:47:12 -0500

    frontend restart

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const { Pool } = require('pg');  // Import the PostgreSQL client
 
 const app = express();
-const PORT = 5000;
+const PORT = 7000;
 
 // Set up the PostgreSQL connection pool
 const pool = new Pool({
